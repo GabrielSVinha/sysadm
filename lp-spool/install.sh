e29231df35ebd0975370b02b079805f37796a5b6
GabrielSVinha <gabriel.vinha@ccc.ufcg.edu.br>
2018-10-31 17:26:48 -0300

    Add checkings for lp

--- lp-spool/install.sh
+++ lp-spool/install.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit 1;
-fi
+#if [ "$EUID" -ne 0 ]
+#  then echo "Please run as root"
+#  exit 1;
+#fi
 
 PAG=$1
 ARQ=$2
 
-mkdir -p ./lp/
+echo "Creating configuration file"
 
-awk -v PAG=$PAG -v ARQ=$ARQ 'BEGIN{FS=":";OFS=":"}{print $1 " " PAG " " ARQ}' /etc/passwd > ./lp/$(date +"%b-%Y").conf
+mkdir -p ./lp-dir/
+
+awk -v PAG=$PAG -v ARQ=$ARQ 'BEGIN{FS=":";OFS=":"}{print $1 " " PAG " " ARQ " " 0}' /etc/passwd > ./lp-dir/$(date +"%b-%Y").conf
+
+echo "Installing lp tool"
 
