92d209148dbb6166a261fd5c29841e0330d675f4
adbys <adbys>
2018-11-05 18:44:02 -0300

    refres-quota and report

--- lp-spool/install.sh
+++ lp-spool/install.sh
@@ -14,8 +14,8 @@ mkdir -p ./lp-dir/
 mkdir -p ./lp-dir/report
 mkdir -p ./lp-dir/config
 
-echo "PAGE_LIMIT: $PAG" > ./lp-dir/config/limits.conf
-echo "FILE_LIMIT: $ARQ" >> ./lp-dir/config/limits.conf
+echo "PAGE_LIMIT: $PAG" > ./lp-dir/config/limits
+echo "FILE_LIMIT: $ARQ" >> ./lp-dir/config/limits
 
 awk -v PAG=$PAG -v ARQ=$ARQ 'BEGIN{FS=":";OFS=":"}{print $1 " " PAG " " ARQ " " 0}' /etc/passwd > ./lp-dir/config/$(date +"%b-%Y").conf
 
